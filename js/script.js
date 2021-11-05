40079f98b89b0843e6a8bd99647056a5387bdd28
taalai123 <tako250494@gmail.com>
2021-11-05 13:23:19 +0600

    initial

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $(".owl-carousel").owlCarousel({
+        nav: true,
+        items: 1,
+        loop: true,
+        autoWidth: true,
+        autoplay: true,
+        navText: ["<div class='nav-button owl-prev'>‹</div>", "<div class='nav-button owl-next'>›</div>"],
+
+    });
+});
